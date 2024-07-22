d48a1379c7c5313c51ec516c4751044e3ccf08cc
Viratrajesh <115334651+Viratrajesh@users.noreply.github.com>
2024-07-22 10:47:18 +0530

    info.py

--- info.py
+++ info.py
@@ -43,8 +43,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Rajesh1162005:<Rajesh18>@cluster0.elblpo5.mongodb.net/")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Rajesh1162005")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
